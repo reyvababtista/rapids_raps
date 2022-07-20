8c7aece19db5f9c1eb4a8915a2a672e65f56b78e
reyvababtista <reyvababtista16@gmail.com>
2022-07-19 21:26:52 -0500

    fix: fix schema

--- src/data/streams/pull_raps_data.R
+++ src/data/streams/pull_raps_data.R
@@ -3,7 +3,7 @@ source("renv/activate.R")
 library(yaml)
 library(dplyr)
 library(readr)
-# we use reticulate but only load it if we are going to use it to minimize the case when old RAPIDS deployments need to update ther renv
+# we use reticulate but only load it if we are going to use it to minimize the case when old REY deployments need to update ther renv
 
 validate_deviceid_platforms <- function(device_ids, platforms, participant){
   if(length(device_ids) > 1 && length(platforms) == 1){
@@ -61,10 +61,10 @@ rename_columns <- function(name_maps, data){
 validate_expected_columns_mapping <- function(schema, rapids_schema, sensor, rapids_schema_file, stream_format){
   rapids_columns <- rapids_schema[[sensor]]
   if(is.null(rapids_columns))
-    stop(paste(sensor, " columns are not listed in RAPIDS' column specification. If you are adding support for a new RAPS sensor, add any mandatory columns in ", rapids_schema_file))
+    stop(paste(sensor, " columns are not listed in REY' column specification. If you are adding support for a new RAPS sensor, add any mandatory columns in ", rapids_schema_file))
   
   if("ANDROID" %in% schema[[sensor]]){
-    android_columns <- names(schema[[sensor]][["ANDROID"]][["RAPIDS_COLUMN_MAPPINGS"]])
+    android_columns <- names(schema[[sensor]][["ANDROID"]][["REY_COLUMN_MAPPINGS"]])
     if(length(setdiff(rapids_columns, android_columns)) > 0)
       stop(paste(sensor," mappings are missing one or more mandatory columns for ANDROID. The missing column mappings are for ", paste(setdiff(rapids_columns, android_columns), collapse=","),"in", stream_format, " (the mappings are case sensitive)"))
     if(length(setdiff(android_columns, rapids_columns)) > 0)
@@ -72,8 +72,7 @@ validate_expected_columns_mapping <- function(schema, rapids_schema, sensor, rap
   }
 
   if("IOS" %in% schema[[sensor]]){
-    ios_columns <- names(schema[[sensor]][["IOS"]][["RAPIDS_COLUMN_MAPPINGS"]])
-    if(length(setdiff(rapids_columns, ios_columns)) > 0)
+    ios_columns <- names(schema[[sensor]][["IOS"]][["REY_COLUMN_MAPPINGS"]]) if(length(setdiff(rapids_columns, ios_columns)) > 0)
       stop(paste(sensor," mappings are missing one or more mandatory columns for IOS. The missing column mappings are for ", paste(setdiff(rapids_columns, ios_columns), collapse=","),"in", stream_format, " (the mappings are case sensitive)"))
     if(length(setdiff(ios_columns, rapids_columns)) > 0)
       stop(paste(sensor," mappings have one or more columns than required for IOS. The extra column mappings are for ", paste(setdiff(ios_columns, rapids_columns), collapse=","),"in", stream_format, " (the mappings are case sensitive)"))
@@ -136,7 +135,7 @@ pull_raps_data <- function(){
     device_oss <- rep(device_oss, length(devices))
 
   validate_expected_columns_mapping(stream_schema, rapids_schema, sensor, rapids_schema_file, stream_format)
-  # ANDROID or IOS RAPIDS_COLUMN_MAPPINGS are guaranteed to be the same at this point (see validate_expected_columns_mapping function)
+  # ANDROID or IOS REY_COLUMN_MAPPINGS are guaranteed to be the same at this point (see validate_expected_columns_mapping function)
   expected_columns <- tolower(rapids_schema[[sensor]])
   participant_data <- setNames(data.frame(matrix(ncol = length(expected_columns), nrow = 0)), expected_columns)
 
@@ -165,12 +164,12 @@ pull_raps_data <- function(){
 
     os_table <- ifelse(length(tables) > 1, tables[[toupper(device_os)]], tables) # some sensor tables have a different name for android and ios    
 
-    columns_to_download <- c(stream_schema[[sensor]][[toupper(device_os)]][["RAPIDS_COLUMN_MAPPINGS"]], stream_schema[[sensor]][[toupper(device_os)]][["MUTATION"]][["COLUMN_MAPPINGS"]])
+    columns_to_download <- c(stream_schema[[sensor]][[toupper(device_os)]][["REY_COLUMN_MAPPINGS"]], stream_schema[[sensor]][[toupper(device_os)]][["MUTATION"]][["COLUMN_MAPPINGS"]])
     columns_to_download <- columns_to_download[(columns_to_download != "FLAG_TO_MUTATE")]
     data <- pull_data_container(data_configuration, device, sensor, os_table, columns_to_download)
     
     if(!setequal(columns_to_download, colnames(data)))
-      stop(paste0("The pulled data for ", device, " does not have the expected columns (including [RAPIDS_COLUMN_MAPPINGS] and [MUTATE][COLUMN_MAPPINGS]). The container script returned [", paste(colnames(data), collapse=","),"] but the format mappings expected [",paste(columns_to_download, collapse=","), "]. The conainer script is: ", stream_container))
+      stop(paste0("The pulled data for ", device, " does not have the expected columns (including [REY_COLUMN_MAPPINGS] and [MUTATE][COLUMN_MAPPINGS]). The container script returned [", paste(colnames(data), collapse=","),"] but the format mappings expected [",paste(columns_to_download, collapse=","), "]. The conainer script is: ", stream_container))
     
     renamed_data <- rename_columns(columns_to_download, data)
     
@@ -178,7 +177,7 @@ pull_raps_data <- function(){
     mutated_data <- mutate_data(mutation_scripts, renamed_data, data_configuration)
 
     if(!setequal(expected_columns, colnames(mutated_data)))
-      stop(paste0("The mutated data for ", device, " does not have the columns RAPIDS expects. The mutation script returned [", paste(colnames(mutated_data), collapse=","),"] but RAPIDS expected [",paste(expected_columns, collapse=","), "]. One ore more mutation scripts in [", sensor,"][MUTATION][SCRIPTS] are adding extra columns or removing or not adding the ones expected"))
+      stop(paste0("The mutated data for ", device, " does not have the columns REY expects. The mutation script returned [", paste(colnames(mutated_data), collapse=","),"] but REY expected [",paste(expected_columns, collapse=","), "]. One ore more mutation scripts in [", sensor,"][MUTATION][SCRIPTS] are adding extra columns or removing or not adding the ones expected"))
     participant_data <- rbind(participant_data, mutated_data %>% distinct())
       
   }
